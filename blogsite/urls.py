f23a0cd78d016f70998a3041d267a85c4a2400f3
Shubham Sood <41718296+ShubhamSood1406@users.noreply.github.com>
2020-05-30 16:18:57 +0530

    Update urls.py

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -5,5 +5,7 @@ from blogapp import views       #imporing views file from blogapp directory
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
-    path('addblog/', views.createBlog, name  = "createBlog"),       #path of url after adding blog
+    
+    #url path after adding blog
+    path('addblog/', views.createBlog, name  = "createBlog"),
 ]
